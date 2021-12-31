256b716661414134444261dee320fdf8b72e2326
rakibul-wdp <rakibul.wdp@gmail.com>
2022-01-01 01:39:00 +0600

    create contact page without components

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,99 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import styled from "styled-components";
+
+const Contact = () => {
+  const { isAuthenticated, user } = useAuth0();
+
+  const Wrapper = styled.section`
+    padding: 9rem 0 5rem 0;
+    text-align: center;
+
+    .container {
+      margin-top: 6rem;
+
+      .contact-form {
+        max-width: 50rem;
+        margin: auto;
+
+        .contact-inputs {
+          display: flex;
+          flex-direction: column;
+          gap: 3rem;
+
+          input[type="submit"] {
+            max-width: 50rem;
+            cursor: pointer;
+            transition: all 0.2s;
+
+            &:hover {
+              background-color: ${({ theme }) => theme.colors.white};
+              border: 1px solid ${({ theme }) => theme.colors.btn};
+              color: ${({ theme }) => theme.colors.btn};
+              transform: scale(0.9);
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  return (
+    <Wrapper>
+      <h2 className="common-heading" style={{ marginTop: "-3rem" }}>
+        Contact with us
+      </h2>
+
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d7252.222508415827!2d89.39886162704492!3d24.654298586606103!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39fdb32dfd789943%3A0xe7f2b001cc8988c1!2sMamursahi!5e0!3m2!1sen!2sbd!4v1674903560564!5m2!1sen!2sbd"
+        title="location"
+        width="100%"
+        height="400"
+        style={{ border: 0 }}
+        allowFullScreen=""
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
+
+      <div className="container">
+        <div className="contact-form">
+          <form
+            action="https://formspree.io/f/meqweand"
+            method="POST"
+            className="contact-inputs"
+          >
+            <input
+              type="text"
+              placeholder="username"
+              name="username"
+              value={isAuthenticated ? user.name : ""}
+              required
+              autoComplete="off"
+            />
+
+            <input
+              type="email"
+              name="Email"
+              placeholder="Email"
+              value={isAuthenticated ? user.email : ""}
+              autoComplete="off"
+              required
+            />
+
+            <textarea
+              name="Message"
+              cols="30"
+              rows="10"
+              required
+              autoComplete="off"
+              placeholder="Enter you message"
+            ></textarea>
+
+            <input type="submit" value="send" />
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Contact;
