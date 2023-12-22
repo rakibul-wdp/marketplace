22e18fa16abad4fbf398951472f71ff8b1c3a9d7
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-23 02:39:00 +0600

    create refreshToken functionaltiy

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -106,3 +106,38 @@ export const getUser = async (req, res, next) => {
   }
   return res.status(200).json({ user });
 };
+
+export const refreshToken = (req, res, next) => {
+  const cookies = req.headers.cookie;
+  const prevToken = cookies.split("=")[1];
+  if (!prevToken) {
+    return res.status(400).json({ message: "Couldn't find token" });
+  }
+  jwt.verify(
+    String(prevToken),
+    process.env.process.env.JWT_SECRET_KEY,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        return res.status(403).json({ message: "Authentication failed" });
+      }
+      res.clearCookie(`${user.id}`);
+      req.cookies[`${user.id}`] = "";
+
+      const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET_KEY, {
+        expiresIn: "60s",
+      });
+      console.log("Regenerated Token\n", token);
+
+      res.cookie(String(user.id), token, {
+        path: "/",
+        expires: new Date(Date.now() + 1000 * 60),
+        httpOnly: true,
+        sameSite: "lax",
+      });
+
+      req.id = user.id;
+      next();
+    }
+  );
+};
