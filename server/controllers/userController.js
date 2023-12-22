dba1d6c260b1d62fce851a411dee8850ee5f3f41
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-23 03:39:00 +0600

    create logout functionaltiy

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -141,3 +141,24 @@ export const refreshToken = (req, res, next) => {
     }
   );
 };
+
+export const logout = (req, res, next) => {
+  const cookies = req.headers.cookie;
+  const prevToken = cookies.split("=")[1];
+  if (!prevToken) {
+    return res.status(400).json({ message: "Couldn't find token" });
+  }
+  jwt.verify(
+    String(prevToken),
+    process.env.process.env.JWT_SECRET_KEY,
+    (err, user) => {
+      if (err) {
+        console.log(err);
+        return res.status(403).json({ message: "Authentication failed" });
+      }
+      res.clearCookie(`${user.id}`);
+      req.cookies[`${user.id}`] = "";
+      return res.status(200).json({ message: "Successfully Logged Out" });
+    }
+  );
+};
