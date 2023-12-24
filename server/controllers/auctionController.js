f79f936dca90e76442717da1d68da13a93f6a600
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-25 03:39:00 +0600

    create all auction item functionaltiy

--- server/controllers/auctionController.js
+++ server/controllers/auctionController.js
@@ -41,3 +41,40 @@ export const auctionItem = async (req, res, next) => {
     return res.status(500).json({ message: "Error adding item to auction" });
   }
 };
+
+export const allAuctionItem = async (req, res, next) => {
+  try {
+    const allUsers = await User.find({});
+
+    if (!allUsers || allUsers.length === 0) {
+      return res.status(404).json({ message: "No users or items found" });
+    }
+
+    let auctionedItemsWithUserDetails = [];
+
+    allUsers.forEach((user) => {
+      if (user.itemList && user.itemList.length > 0) {
+        const auctionedItems = user.itemList.filter(
+          (item) => item.isAuctioned === true
+        );
+        if (auctionedItems.length > 0) {
+          auctionedItems.forEach((item) => {
+            const itemWithUserDetails = {
+              userId: user._id,
+              itemId: item._id,
+            };
+            auctionedItemsWithUserDetails.push(itemWithUserDetails);
+          });
+        }
+      }
+    });
+
+    if (auctionedItemsWithUserDetails.length === 0) {
+      return res.status(404).json({ message: "No auctioned items available" });
+    }
+
+    res.status(200).json(auctionedItemsWithUserDetails);
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching auctioned items" });
+  }
+};
