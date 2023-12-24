b6ef3ffcbe4d63465daadd4002201cc672fac9f2
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-25 01:39:00 +0600

    create all product functionaltiy

--- server/controllers/itemController.js
+++ server/controllers/itemController.js
@@ -30,3 +30,29 @@ export const addItemList = async (req, res, next) => {
 
   res.status(201).json({ message: "Item added successfully" });
 };
+
+export const allProduct = async (req, res, next) => {
+  try {
+    const allUsers = await User.find({});
+
+    if (!allUsers) {
+      return res.status(404).json({ message: "No user exist" });
+    }
+
+    let allProduct = [];
+
+    allUsers.forEach((user) => {
+      if (user.itemList && user.itemList.length > 0) {
+        allProduct = allProduct.concat(user.itemList);
+      }
+    });
+
+    if (allProduct.length === 0) {
+      return res.status(404).json({ message: "No product available" });
+    }
+
+    res.status(201).json(allProduct);
+  } catch (err) {
+    res.status(500).json({ message: "Error fetching products" });
+  }
+};
