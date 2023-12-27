59da8ae86c39b4725fa0ce8f1a39bd2f1bb82d39
rakibul-wdp <rakibul.wdp@gmail.com>
2023-12-28 03:39:00 +0600

    import all component and pages in app file with route setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
+import { useSelector } from "react-redux";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  console.log(isLoggedIn);
+
   return (
-    <main>
-      <h1>Hello World</h1>
-    </main>
+    <>
+      <header>
+        <Header />
+      </header>
+
+      <main>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<SignUp />} />
+          {isLoggedIn && <Route path="/user" element={<Home />} />}
+        </Routes>
+      </main>
+    </>
   );
 }
 
